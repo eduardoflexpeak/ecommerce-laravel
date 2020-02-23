f48183b4461bcc1cfc04522596b6468f25714100
Eduardo <eduardo@dudstecnologia.com.br>
2020-02-23 07:04:30 -0400

    Remoção de Pacote utilizado para teste

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,7 +12,6 @@ try {
 
     require('overlayscrollbars');
     require('../../vendor/almasaeed2010/adminlte/dist/js/adminlte');
-    require('sweetalert2');
 
     require('bootstrap');
 } catch (e) {}
